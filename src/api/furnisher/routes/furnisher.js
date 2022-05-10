bbd166a1cbc7d00fed61ec2126a0dc9bab2e328a
Edmundo Paulino <manyymoore@gmail.com>
2022-05-10 17:05:42 -0300

    chore: 🗃️ Database settings

---
+++ src/api/furnisher/routes/furnisher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * furnisher router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::furnisher.furnisher');
