fc6c2e83c1eeaa25f3e0dfa914a0d8a3cc54db7a
Edmundo Paulino <manyymoore@gmail.com>
2022-05-11 15:52:29 -0300

    refactor: 🚚 Add new route, fix population

--- src/api/client/controllers/client.js
+++ src/api/client/controllers/client.js
@@ -12,16 +12,17 @@ module.exports = createCoreController("api::client.client", ({ strapi }) => ({
       const email = ctx.state?.user?.email;
 
       if (email) {
-        ctx.query = {
-          populate: "*",
-          filters: { email: { $eq: email } },
-        };
+        const entry = strapi.db.query("api::client.client").findOne({
+          select: ["id", "description", "code", "cnpj", "email"],
+          where: { email },
+          populate: {
+            accounts: { select: ["code", "description"] },
+            furnishers: { select: ["code", "description"] },
+            histories: { select: ["code", "description"] },
+          },
+        });
 
-        const {
-          data: [data],
-        } = await super.find(ctx);
-
-        return data;
+        if (entry) return entry;
       }
     } catch (err) {
       return err;
@@ -37,9 +38,7 @@ module.exports = createCoreController("api::client.client", ({ strapi }) => ({
           where: { code },
         });
 
-        if (entry) {
-          return entry;
-        }
+        if (entry) return entry;
       }
     } catch (err) {
       return err;
