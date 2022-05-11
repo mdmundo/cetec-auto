a0a21b12d64082f05b4b6bc969a9dfac39af7821
Edmundo Paulino <manyymoore@gmail.com>
2022-05-11 12:56:52 -0300

    refactor: ✨ Creation of custom route to show client profile

--- src/api/client/controllers/client.js
+++ src/api/client/controllers/client.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  *  client controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::client.client');
+module.exports = createCoreController("api::client.client", ({ strapi }) => ({
+  async profile(ctx) {
+    try {
+      const email = ctx.state?.user?.email;
+
+      if (email) {
+        const entry = await strapi.db.query("api::client.client").findOne({
+          select: ["email"],
+          where: { email },
+        });
+
+        if (entry) {
+          ctx.body = entry;
+        }
+      }
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
