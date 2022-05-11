9e55abae71f831e3ea90fd41948e9eb6aad241fe
Edmundo Paulino <manyymoore@gmail.com>
2022-05-11 09:51:19 -0300

    chore: 🔇 Remove log

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -10,7 +10,6 @@ module.exports = ({ env }) => ({
     tasks: {
       robot: {
         task: ({ strapi }) => {
-          console.log("I'm gonna call the bot right now...");
           /* Call robot with axios sending everything on queue */
           /* Robot should send another request to indicate success that will be used to clear queue */
         },
