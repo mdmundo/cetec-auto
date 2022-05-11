6bd1de8d98e8caac8d6661c4a443acf69bfa2830
Edmundo Paulino <manyymoore@gmail.com>
2022-05-11 08:40:20 -0300

    refactor: 🚧 Improve Dockerfile, add cronjob and edit env vars example

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,24 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", ""),
+  url: env("PUBLIC_URL"),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  cron: {
+    enabled: env.bool("CRON_ENABLED", false),
+    tasks: {
+      robot: {
+        task: ({ strapi }) => {
+          console.log("I'm gonna call the bot right now...");
+          /* Call robot with axios sending everything on queue */
+          /* Robot should send another request to indicate success that will be used to clear queue */
+        },
+        options: {
+          rule: "0 0 0 * * *",
+          tz: "America/Araguaina",
+        },
+      },
+    },
   },
 });
