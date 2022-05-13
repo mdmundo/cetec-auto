dd70f963ba9c646f540e4dcb5ed6c666e885b7ba
Edmundo Paulino <manyymoore@gmail.com>
2022-05-13 06:35:01 -0300

    refactor: 🚧 Migrating from relations to json

--- src/api/furnisher/routes/furnisher.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * furnisher router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::furnisher.furnisher');
